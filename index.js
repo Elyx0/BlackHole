6a340f52c1a1deebc4ee8fd1f309be085dc62ad1
Elyx0 <Elyx00@gmail.com>
2015-11-12 17:15:08 -0500

    npm

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
   var express = require('express');
   var app = express();
-  app.use(express.static('public'));
-  app.use(express.static('js'));
-  app.use(express.static('assets'));
+  app.use(express.static('./public'));
+  app.use(express.static('./js'));
+  app.use(express.static('./assets'));
   app.listen(process.env.PORT || 4001);
   console.log('Server Started.');
